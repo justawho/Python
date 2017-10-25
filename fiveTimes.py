e1746e056837cbcd7adc56efff19f43acf3572ed
Mikhail "Justin"-Guy Lessey-Hordatt <justin.lesseyhordatt@gmail.com>
2017-10-25 10:03:08 -0600

    Included while loop
    did the same thing with while loop

--- fiveTimes.py
+++ fiveTimes.py
@@ -1,3 +1,11 @@
 print('My name is')
 for i in range(5):
     print('Jimmy Five Times ('+ str(i) + ')')
+
+##doing the same thing about with a while look
+
+print('My name is')
+i= 0
+while i < 5:
+    print ('Jimmy Five Times(' + str(i)+ ')')
+    i = i +1
