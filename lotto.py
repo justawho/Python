8196246874e3c5fb22c86b877c4e28036f9f3f77
Mikhail "Justin"-Guy Lessey-Hordatt <justin.lesseyhordatt@gmail.com>
2017-11-09 13:52:33 -0700

    Fixed the lotto.
    Numbers are properly generated and added to list. no dupes

--- lotto.py
+++ lotto.py
@@ -3,19 +3,23 @@ import random
 
 def lotto():
     number = [0] * 8
-    print (number)
+    
     for i in range (8):
         current = random.randint(1, 54)
-        print (current)
+        
         if  current not in number:
             number[i] = current
-            print (number)
+            
         else:
             current = random.randint(1, 54)
             number[i] = current
-    print (number)
     powerball = random.randint(1, 24)
+    print ((number) ,)
     print ('powerball number: ' + str(powerball))
 
 
+lotto()
+lotto()
+lotto()
+lotto()
 lotto()
