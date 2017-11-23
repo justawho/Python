d416293fd98bc2c6e33f41f4f06272d43baa6171
Mikhail "Justin"-Guy Lessey-Hordatt <justawho@users.noreply.github.com>
2017-11-23 13:17:02 -0700

    fixt
    breaking and unbreaking for loop.

--- inventory.py
+++ inventory.py
@@ -10,7 +10,7 @@ stuff = {'rope': 1, 'torch':6, 'gold coin': 42, 'dagger': 1, 'arrow':12}
 def displayInventory(inventory):
     print ("Inventory:")
     itemTotal = 0
-    for  k, v in inventory.values():
+    for  k, v in inventory.items():
         itemTotal = itemTotal + v
         print( str(v) +' '+ str(k))
     print("Total number of items: " + str(itemTotal))
