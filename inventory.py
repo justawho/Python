d283f988d73d10d3ae007f40e4be2912fe43a608
Mikhail "Justin"-Guy Lessey-Hordatt <justawho@users.noreply.github.com>
2017-11-24 12:36:57 -0700

    addToInvetory function addded
    added a function named addToInventory(inventory, addedItems), wheere teh inventory parameter is adictionary represetning the player's inventory and the addedItem parameter is a list. The addToInventory function can return a dictionary that represents the updated inventory.

--- inventory.py
+++ inventory.py
@@ -1,19 +1,41 @@
-##inventory.py
-
-##Modeling player inventory with a dictionary. 
-
-
-stuff = {'rope': 1, 'torch':6, 'gold coin': 42, 'dagger': 1, 'arrow':12}
+# inventory.py
+# Modeling player inventory with a dictionary.
 
 
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+
+# runs through a users dictionary inventory and dispalys an each item and its quantity followed by the total number of
+# items in the inventory.
 def displayInventory(inventory):
     print ("Inventory:")
     itemTotal = 0
-    for  k, v in inventory.items():
+    for k, v in inventory.items():
         itemTotal = itemTotal + v
-        print( str(v) +' '+ str(k))
+        print(str(v) + ' ' + str(k))
     print("Total number of items: " + str(itemTotal))
+
 
 displayInventory(stuff)
 
+
+# update inventory numbers or adds items to inventory after looting
+def addToInventory(inventory, addedItems):
+    for i in set(addedItems):
+        if i in inventory:
+            inventory[i] = inventory[i] + addedItems.count(i)
+        else:
+            inventory.setdefault(i, addedItems.count(i))
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+stuff = addToInventory(stuff, dragonLoot)
+print('//////////////////////////////////////////////////////////')
+print('Inv print out :')
+displayInventory(inv)
+print('////////////////////////////////////////////////////////////')
+print('Stuff print out:')
+displayInventory(stuff)
