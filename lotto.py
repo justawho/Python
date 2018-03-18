f8c116a3ff48510ab914ec91e0d762fedb0be544
Mikhail "Justin"-Guy Lessey-Hordatt <justawho@users.noreply.github.com>
2018-03-18 08:20:47 -0600

    Don't reinvent the wheel
    Streamlined the random number picking process. no longer need for statements. ez pezy boi

--- lotto.py
+++ lotto.py
@@ -2,17 +2,8 @@ import random
 
 
 def lotto():
-    number = [0] * 8
+    number = random.sample(range(1, 69), 5)
     
-    for i in range (8):
-        current = random.randint(1, 54)
-        
-        if  current not in number:
-            number[i] = current
-            
-        else:
-            current = random.randint(1, 54)
-            number[i] = current
     powerball = random.randint(1, 24)
     print ((number) ,)
     print ('powerball number: ' + str(powerball))
