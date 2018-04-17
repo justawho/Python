e5e46cce1b835e80fa642dd16309e5499ffe38b2
Mikhail "Justin"-Guy Lessey-Hordatt <justin.lesseyhordatt@gmail.com>
2018-04-17 12:47:22 -0600

    quizes and answers
    completed scripts and generated the quiz and answer files

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -59,13 +59,43 @@ capitals = {'Alabama': 'Montgomery',
 #Generate 35 quiz files.
 
 for quizNum in range(35):
-    #TODO: Create the quiz and answer key files.
-
-    #TODO: Write out the header for the quiz.
-
-    #TODO: Shuffle the order of the states.
-
-    #TODO: Loop through all 50 states, making a question for each.
+    # Create the quiz and answer key files.
     
+    quizFile = open('capitalsquiz%s.txt' %(quizNum +1), 'w')
+    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+    # Write out the header for the quiz.
+
+    quizFile.write('Name:\n\nDate: \n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    # Shuffle the order of the states.
+
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    # Loop through all 50 states, making a question for each.
+
+    for questionNum in range(50):
+        # Get right and wrong answers.
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+    # Write the question and answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write(' %s. %s\n' %('ABCD'[i], answerOptions[i]))
+            quizFile.write('\n')
+
+
+    #TODO: write the answer key to a file.
+            answerKeyFile.write('%s. %s\n' %(questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
             
             
