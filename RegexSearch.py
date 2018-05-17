b45f203b8d65fb2a41b3f11463f8338fb8d03284
Mikhail "Justin"-Guy Lessey-Hordatt <justin.lesseyhordatt@gmail.com>
2018-05-17 09:11:16 -0600

    Regex search start up
    initial comments and import statement added.

---
+++ RegexSearch.py
@@ -0,0 +1,7 @@
+#! python3
+## RegexSearch.pw
+## A program that opens all .txt files in a folder and searches for any line
+## that matches a user supplied regular expression. The result will be
+## printed to the screen.
+
+import re, os
