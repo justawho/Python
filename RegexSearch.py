0010c7c4bb00e8bc4f77de5359faaceea1d623a2
Mikhail "Justin"-Guy Lessey-Hordatt <justin.lesseyhordatt@gmail.com>
2018-05-22 12:38:33 -0600

    search complete.
    completed the regexsearch.py program, not sure if I am happy with the
    product yet.

--- RegexSearch.py
+++ RegexSearch.py
@@ -4,4 +4,27 @@
 ## that matches a user supplied regular expression. The result will be
 ## printed to the screen.
 
-import re, os
+import re, os, glob
+
+
+def regFinder(fileName, regex):
+    userRegex = re.compile((regex), re.I)
+    results = userRegex.search(fileName)
+    entry = re.split(regex, fileName)
+    print (entry)
+
+os.chdir('C:\\Users\\mjlesseyhordatt\\Documents\\GitHub\\Python\\regexSearch')
+regex = input("Please enter what you would like to search for: ")
+
+for file in glob.glob('./*.txt'):
+    print(file)
+    filer = open(file, 'r')
+    fileContent = filer.readlines()
+    for i in  range(len(fileContent)):
+        line = str(i)
+        if regex in fileContent[i].lower():
+            print ("Input can be found on line: "+ line  )
+            print (fileContent[i])
+            
+
+            
