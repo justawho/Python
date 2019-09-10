dd70bd9da62ff0864ba1de440e85923d623ceade
Mikhail "Justin"-Guy Lessey-Hordatt <justawho@users.noreply.github.com>
2019-09-10 09:10:53 -0600

    Update lotto.py
    Numbers populate in the GUI after button push

--- lotto.py
+++ lotto.py
@@ -2,12 +2,13 @@ import random
 from tkinter import *
 
 def lotto():
-    
+
     numbers = sorted(random.sample(range(1, 69), 5))
     numString = ', '.join(map(str,numbers))
     powerball = random.randint(1, 24)
-    txtOutput = "White Balls : " + numString + '\n' + 'Powerball : ' + str(powerball) 
+    txtOutput = "White Balls : " + numString + '\n' + 'Powerball : ' + str(powerball)
     print(txtOutput +  '\n')
+    TheNumbers.config(text=txtOutput)
     return (numbers)
 
 
@@ -18,10 +19,12 @@ def lotto():
 root = Tk()
 
 
-theLabel = Label(root)
-theLabel.printButton = Button(root, text = "Get Numbers", command = lambda:lotto())
-theLabel.printButton.pack()
+theLabel = Label(root,text ="Guess the numbers")
+TheNumbers = Label(root, text ="Guess the numbers")
+TheNumbers.pack()
+theLabel.printButton = Button(root, text = "Get Numbers", command = lambda:lotto()).pack()
+
+
+
 
 root.mainloop()
-
-
