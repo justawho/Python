39720e7e32701a0de0cd78cfda700cbac3991b55
Mikhail "Justin"-Guy Lessey-Hordatt <justawho@users.noreply.github.com>
2020-03-07 15:09:25 -0700

    trying to update gui with information

--- lotto.py
+++ lotto.py
@@ -1,15 +1,17 @@
 import random
+import time
 from tkinter import *
+
+root = Tk()
+theLabel = Label(root,text ="Guess the numbers")
+TheNumbers = Label(root, text ="Guess the numbers")
 
 def lotto():
 
     numbers = sorted(random.sample(range(1, 69), 5))
     numString = ', '.join(map(str,numbers))
     powerball = random.randint(1, 24)
-    txtOutput = "White Balls : " + numString + '\n' + 'Powerball : ' + str(powerball)
- 
- 
-       
+    txtOutput = "White Balls : " + numString + '\n' + 'Powerball : ' + str(powerball)     
     return (txtOutput)
 
 
@@ -18,32 +20,39 @@ def lottodraw():
     drawnumString = ', '.join(map(str,drawnumbers))
     drawpowerball = random.randint(1, 24)
     drawtxtOutput = "White Balls : " + drawnumString + '\n' + 'Powerball : ' + str(drawpowerball)
-   
-
     return (drawtxtOutput)
 
 def theDraw():
     i = 0
 
     yourPick = lotto()
+    TheNumbers.config (text=str(yourPick))
+    TheNumbers.pack()
     drawPick = lottodraw()
 
     print (yourPick)
+    theLabel.config(text= "The next statement will be the number of times the while statement ran before a match was made")
+    theLabel.pack()
     print ("The next statement will be the number of times the while statement ran before a match was made")
-
+    time.sleep(.300)
     while yourPick != drawPick:
         drawPick = lottodraw()
         i = i+1
 
-    print (i)
+    theLabel.config(text= "It took " + str(i) + " draws to hit the numbers picked.")
+    theLabel.pack()
     print (drawPick)
     return ("It took " + str(i) + " draws to hit the numbers picked.")
 
-root = Tk()
-theLabel = Label(root,text ="Guess the numbers")
-TheNumbers = Label(root, text ="Guess the numbers")
+
+theLabel.pack()
+
+#root = Tk()
+#theLabel = Label(root,text ="Guess the numbers")
+
 TheNumbers.pack()
-theLabel.printButton = Button(root, text = "Get Numbers", command = lambda:theDraw()).pack()
+theLabel.printButton = Button(root, text = "Get Numbers", command = theDraw).pack()
+
 
 
 
